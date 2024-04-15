9bca0a26107d5e62c80cd5b43111102efe259392
Zeanon <thezeanon@gmail.com>
2024-04-15 14:58:42 +0200

    Update configuration.py

--- bot/configuration.py
+++ bot/configuration.py
@@ -378,11 +378,11 @@ class TelegramUIConfig(ConfigHelper):
         if self.hide_macros:
             for line in self.buttons:
                 for i in range(0, line.count("/macros")):
-                    line.remove('/macros')
+                    line.remove("/macros")
         if self.hide_files:
             for line in self.buttons:
                 for i in range(0, line.count("/files")):
-                    line.remove('/files')
+                    line.remove("/files")
 
 
 class StatusMessageContentConfig(ConfigHelper):
